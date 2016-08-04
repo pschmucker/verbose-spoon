615fa5f051f76113d068fb062cf448c1e5ef3500
Philippe Schmucker <philippe.schmucker@gmail.com>
2016-08-04 23:45:57 +0200

    Add validator.ts sample

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import './validation-sample';
+
 @Component({
   selector: 'my-app',
   template: '<h1>My First Angular 2 App with Webpack</h1>'
---
+++ app/validation-sample.ts
@@ -0,0 +1,38 @@
+import {Validator} from "validator.ts/Validator";
+import {Contains, IsInt, IsLength, IsEmail, IsFQDN, IsDate} from "validator.ts/decorator/Validation";
+ 
+export class Post {
+ 
+    @IsLength(10, 20)
+    title: string;
+ 
+    @Contains('hello')
+    text: string;
+ 
+    @IsInt({ min: 0, max: 10 })
+    rating: number;
+ 
+    @IsEmail()
+    email: string;
+ 
+    @IsFQDN()
+    site: string;
+ 
+    @IsDate()
+    createDate: Date;
+ 
+}
+ 
+let post = new Post();
+post.title = 'Hello'; // should not pass 
+post.text = 'this is a great post about hell world'; // should not pass 
+post.rating = 11; // should not pass 
+post.email = 'google.com'; // should not pass 
+post.site = 'googlecom'; // should not pass 
+ 
+let validator = new Validator();
+let errors = validator.validate(post); // returns you array of errors 
+
+console.log(errors);
+
+
