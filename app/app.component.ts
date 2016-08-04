d68fc12719edd2130f09776eaf02150a9d24087a
Nicolas HENRY <icewil@gmail.com>
2016-08-05 00:06:38 +0200

    reformat app

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import './validation-sample';
 
 @Component({
-  selector: 'my-app',
-  template: '<h1>My First Angular 2 App with Webpack</h1>'
+	selector: 'my-app',
+	template: '<h1>My First Angular 2 App with Webpack</h1>'
 })
 export class AppComponent { }
 
--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,3 @@
-import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { bootstrap } from '@angular/platform-browser-dynamic';
 import { AppComponent } from './app.component';
 bootstrap(AppComponent);
